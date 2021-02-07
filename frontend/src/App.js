1dad2bd6f6915100aa1cd5d05de2775d762c1650
frederico <thecanuckatheist@gmail.com>
2021-02-06 21:48:18 -0500

    updated links to homepage, navbar and products to remove refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
