5fc8a53a6e06ea84aef5107a466f6d8f3398360d
frederico <thecanuckatheist@gmail.com>
2021-02-12 00:07:04 -0500

    seeder.js completion part 2

--- backend/seeder.js
+++ backend/seeder.js
@@ -24,8 +24,34 @@ const importData = async () => {
 
     const sampleProducts = products.map(product => {
       return { ...product, user: adminUser }
-    })
-  } catch (error) {
+    });
 
+    await Product.insertMany(sampleProducts);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`.red.inverse);
+    process.exit(1);
   }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`.red.inverse);
+    process.exit(1);
+  }
+}
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
 }
