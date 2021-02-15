cab8c55d1cfbdcbbebcd565981cf5c1d335cf061
frederico <thecanuckatheist@gmail.com>
2021-02-14 23:17:58 -0500

    updated HomeScreen.js with Message and Error components

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,9 +3,9 @@ import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
   return (
-    <div>
-      
-    </div>
+    <Spinner animation='border' role='status' style={{ width: '100px', height: '100px', margin: 'auto', display: 'block'}}>
+      <span class='sr-only'>Loading...</span>
+    </Spinner>
   )
 };
 
