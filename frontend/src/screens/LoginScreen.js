0911e5e85f6226a9dd3d05f2a14c05ae26a6aac8
frederico <thecanuckatheist@gmail.com>
2021-02-19 23:32:28 -0500

    LoginScreen.js added and functionality part 2

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -7,13 +7,33 @@ import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
 import { login } from '../actions/userActions';
 
-const LoginScreen = () => {
+const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if(userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  }
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
@@ -34,7 +54,7 @@ const LoginScreen = () => {
 
       <Row className='py-3'>
         <Col>
-          New Customer? <Link to={redirect}>Register</Link>
+          New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
         </Col>
       </Row>
     </FormContainer>
