2de40f0bb4851a88db7b2b497332a9f9d0cdd6e2
frederico <thecanuckatheist@gmail.com>
2021-03-14 23:02:51 -0400

    Pagination integrated in frontend part1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,8 @@ function App() {
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/search/:keyword' component={HomeScreen} />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
